3cb04cbc27765c751ce9a97dac84a9487cd0fc2a
B-Lazer <58494463+B-Lazer@users.noreply.github.com>
2023-12-21 23:31:12 -0500

    Update z_onepointdemo.c

--- soh/src/code/z_onepointdemo.c
+++ soh/src/code/z_onepointdemo.c
@@ -98,7 +98,6 @@ s32 OnePointCutscene_SetInfo(PlayState* play, s16 camIdx, s16 csId, Actor* actor
     }
     // #endregion
 
-
     switch (csId) {
         case 1020:
             if (timer < 20) {
@@ -357,15 +356,33 @@ s32 OnePointCutscene_SetInfo(PlayState* play, s16 camIdx, s16 csId, Actor* actor
             Play_SetCameraRoll(play, camIdx, childCam->roll);
             break;
         case 9601:
-            Play_CameraChangeSetting(play, camIdx, CAM_SET_CS_3);
-            Play_CameraChangeSetting(play, MAIN_CAM, mainCam->prevSetting);
-            OnePointCutscene_SetCsCamPoints(csCam, D_80120430 | 0x1000, D_8012042C, D_80120308, D_80120398);
-            break;
+            // #region SOH [Enhancement]
+            if (CVarGetInteger("gCrawlSpeed", 1) > 1) {
+                Play_CameraChangeSetting(play, camIdx, CAM_SET_CS_3);
+                Play_CameraChangeSetting(play, MAIN_CAM, mainCam->prevSetting);
+                OnePointCutscene_SetCsCamPoints(csCam, D_80120430 | 0x1000, camCrawlTimer, D_80120308, D_80120398);
+                break;
+            // #endregion
+            } else {
+                Play_CameraChangeSetting(play, camIdx, CAM_SET_CS_3);
+                Play_CameraChangeSetting(play, MAIN_CAM, mainCam->prevSetting);
+                OnePointCutscene_SetCsCamPoints(csCam, D_80120430 | 0x1000, D_8012042C, D_80120308, D_80120398);
+                break;
+            }
         case 9602:
-            Play_CameraChangeSetting(play, camIdx, CAM_SET_CS_3);
-            Play_CameraChangeSetting(play, MAIN_CAM, mainCam->prevSetting);
-            OnePointCutscene_SetCsCamPoints(csCam, D_80120430 | 0x1000, D_8012042C, D_80120308, D_80120434);
-            break;
+            // #region SOH [Enhancement]
+            if (CVarGetInteger("gCrawlSpeed", 1) > 1) {
+                Play_CameraChangeSetting(play, camIdx, CAM_SET_CS_3);
+                Play_CameraChangeSetting(play, MAIN_CAM, mainCam->prevSetting);
+                OnePointCutscene_SetCsCamPoints(csCam, D_80120430 | 0x1000, camCrawlTimer, D_80120308, D_80120434);
+                break;
+            // #endregion
+            } else {
+                Play_CameraChangeSetting(play, camIdx, CAM_SET_CS_3);
+                Play_CameraChangeSetting(play, MAIN_CAM, mainCam->prevSetting);
+                OnePointCutscene_SetCsCamPoints(csCam, D_80120430 | 0x1000, D_8012042C, D_80120308, D_80120434);
+                break;
+            }
         case 4175:
             csInfo->keyFrames = D_8012147C;
             csInfo->keyFrameCnt = 4;
